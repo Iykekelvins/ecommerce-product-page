6e2a907b8543828515d00ffcdcae051e73e8efc7
Iykekelvins <kelvinochubili@gmail.com>
2021-10-18 14:07:43 +0100

    components for ecommerce product

---
+++ components/index.js
@@ -0,0 +1,12 @@
+//COMPONENTS
+import Navbar from "./Navbar";
+import Button from "./Button";
+import Logo from "./Logo";
+import Seo from "./Seo";
+
+//PRODUCT
+import ProductInfo from "./ProductInfo";
+import ProductImages from "./ProductImages";
+import Cart from "./Cart";
+
+export { Navbar, Button, Logo, Seo, ProductInfo, ProductImages, Cart };
