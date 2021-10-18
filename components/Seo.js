1de8fed1a9729014c137a8d187bc7694e4b09479
Iykekelvins <kelvinochubili@gmail.com>
2021-10-18 14:03:58 +0100

    seo component created

---
+++ components/Seo.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Seo = () => {
+  return (
+    <Head>
+      <title>Create Next App</title>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Frontend Mentor | E-commerce product page</title>
+      <link rel="icon" href="/favicon-32x32.png" />
+    </Head>
+  );
+};
+
+export default Seo;
